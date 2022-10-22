da2621c224d4b00413023313e8ca38c9d80bb37a
Sagar Gurung <gurungsagar6@gmail.com>
2022-10-22 11:31:26 -0700

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Sagar Gurung on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
